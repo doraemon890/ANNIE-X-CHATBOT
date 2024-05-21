a420aecbd3e3d528733907cc9731b332f3e5e8ed
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-21 15:42:56 +0530

    Update and rename gpt to gpt.py

--- ANNIECHATBOT/modules/gpt.py
+++ ANNIECHATBOT/modules/gpt.py
@@ -37,7 +37,7 @@ async def chat_gpt(app, message):
         await message.reply_text(f"**Error**: {e}")
 
 # Command for GPT chat with user's name
-@app.on_message(filters.command(["arvis"], prefixes=["j", "J"]))
+@app.on_message(filters.command(["arvis"], prefixes=["z", "Z"]))
 async def chat_arvis(app, message):
     try:
         await app.send_chat_action(message.chat.id, ChatAction.TYPING)
@@ -55,7 +55,7 @@ async def chat_arvis(app, message):
         await message.reply_text(f"**Error**: {e}")
 
 # Command for ANNIE with user's name
-@app.on_message(filters.command(["nnie"], prefixes=["a", "A"]))
+@app.on_message(filters.command(["nnie"], prefixes=["L", "l"]))
 async def chat_annie(app, message):
     try:
         await app.send_chat_action(message.chat.id, ChatAction.TYPING)
