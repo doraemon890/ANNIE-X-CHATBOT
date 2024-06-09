4a8df2cefd11ee7801800b548d78e8d2af46fd19
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-09 10:20:25 +0530

    Update __init__.py

--- ANNIECHATBOT/database/__init__.py
+++ ANNIECHATBOT/database/__init__.py
@@ -4,6 +4,7 @@ from pyrogram import Client  # Assuming bot is a pyrogram Client instance
 
 # Establish the database connection
 anniedb = MongoClient(config.MONGO_URL)
+annie = anniedb["AnnieDb"]["Annie"]
 chatsdb = anniedb["AnnieDb"]["chats"]
 usersdb = anniedb["AnnieDb"]["users"]
 
