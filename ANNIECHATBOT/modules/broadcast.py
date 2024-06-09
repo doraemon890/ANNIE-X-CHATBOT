fda5074fd13e04d1f9e7a461696da28443356167
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-09 11:24:53 +0530

    Update broadcast.py

--- ANNIECHATBOT/modules/broadcast.py
+++ ANNIECHATBOT/modules/broadcast.py
@@ -1,8 +1,19 @@
 from pyrogram import filters, Client
+from pyrogram.types import Message
 from ANNIECHATBOT import app, OWNER  # Import app and OWNER only
 from ANNIECHATBOT.database.chats import get_served_chats
 from ANNIECHATBOT.database.users import get_served_users
-from pyrogram.types import Message
+
+async def send_media_message(chat_id: int, message: Message):
+    if message.text:
+        await app.send_message(chat_id, message.text)
+    elif message.photo:
+        await app.send_photo(chat_id, photo=message.photo.file_id, caption=message.caption)
+    elif message.video:
+        await app.send_video(chat_id, video=message.video.file_id, caption=message.caption)
+    elif message.document:
+        await app.send_document(chat_id, document=message.document.file_id, caption=message.caption)
+    # Add support for more media types as needed
 
 async def broadcast_message(message: Message):
     served_chats = await get_served_chats()
@@ -27,20 +38,9 @@ async def broadcast_message(message: Message):
 
     return successful_chats, successful_users
 
-async def send_media_message(chat_id: int, message: Message):
-    if message.text:
-        await app.send_message(chat_id, message.text)
-    elif message.photo:
-        await app.send_photo(chat_id, photo=message.photo.file_id, caption=message.caption)
-    elif message.video:
-        await app.send_video(chat_id, video=message.video.file_id, caption=message.caption)
-    elif message.document:
-        await app.send_document(chat_id, document=message.document.file_id, caption=message.caption)
-    # Add support for more media types as needed
-
 @app.on_message(filters.command("broadcast") & filters.user(OWNER))
 async def handle_broadcast(cli: Client, message: Message):
-    if not message.text and not message.photo and not message.video and not message.document:
+    if not any([message.text, message.photo, message.video, message.document]):
         await message.reply_text("Please provide a message or media to broadcast.")
         return
     
