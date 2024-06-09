26d7fa554104f683f939863fad8787da0ddc8ce6
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-09 09:51:25 +0530

    Update __init__.py

--- ANNIECHATBOT/database/__init__.py
+++ ANNIECHATBOT/database/__init__.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
-
 import config
 
+# Establish the database connection
 anniedb = MongoClient(config.MONGO_URL)
-annie = anniedb["AnnieDb"]["Annie"]
+chatsdb = anniedb["AnnieDb"]["chats"]
+usersdb = anniedb["AnnieDb"]["users"]
 
-
+# Import modules to make them available
 from .chats import *
 from .users import *
+from .broadcast import *
