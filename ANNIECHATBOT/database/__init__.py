ca2c2527133ada87122c1077552e882dc6c58498
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-09 10:15:10 +0530

    Update __init__.py

--- ANNIECHATBOT/database/__init__.py
+++ ANNIECHATBOT/database/__init__.py
@@ -7,11 +7,6 @@ anniedb = MongoClient(config.MONGO_URL)
 chatsdb = anniedb["AnnieDb"]["chats"]
 usersdb = anniedb["AnnieDb"]["users"]
 
-# Define the bot
-app = Client("my_bot", api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
-OWNER = config.OWNER_ID
-
 # Import modules to make them available
 from .chats import *
 from .users import *
-from .broadcast import *
