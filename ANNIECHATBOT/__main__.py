185201225510b3664d6c6a693f9672af20ca7530
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-09 12:29:16 +0530

    Update __main__.py

--- ANNIECHATBOT/__main__.py
+++ ANNIECHATBOT/__main__.py
@@ -1,29 +1,26 @@
 import asyncio
 import importlib
-import logging
 
 from pyrogram import idle
 
 from ANNIECHATBOT import LOGGER, app
 from ANNIECHATBOT.modules import ALL_MODULES
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 async def jarvis_boot():
     try:
-        logger.debug("Starting bot...")
         await app.start()
     except Exception as ex:
-        logger.error(f"Failed to start the bot: {ex}")
+        LOGGER.error(ex)
         quit(1)
 
     for all_module in ALL_MODULES:
         importlib.import_module("ANNIECHATBOT.modules." + all_module)
 
-    logger.debug(f"Bot @{app.username} started.")
+    LOGGER.info(f"@{app.username} Started.")
     await idle()
+
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(jarvis_boot())
-    logger.info("Stopping app Bot...")
+    LOGGER.info("Stopping app Bot...")
